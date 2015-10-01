bafff7ccf9aedc0d125300056118101783009404
Shashank Vivek <shashank.093028@gmail.com>
2015-10-01 22:50:47 +0530

    changes in url

--- filereader/urls.py
+++ filereader/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/$', views.loggingin, name='loggingin'),
     #url(r'^homepage/$', views.homepage, name='homepage'),
+    url(r'^getchildren/$', views.getchildren, name='getchildren'),
 ]
